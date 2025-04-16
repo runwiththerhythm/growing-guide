3e0213f4cc0ccce4d2e482a21ef03617d81e8df6
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-04-16 19:25:30 +0100

    Assign variables and get elements from the DOM

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Get elements from the DOM
+const plantGrid = document.getElementById("plantGrid");
+const monthGrid = document.getElementById("monthGrid");
+const modal = document.getElementById("plantModal");
+const modalBody = document.getElementById("modalBody");
+const closeButton = document.querySelector(".close-button");
+const viewByPlantBtn = document.getElementById("viewByPlant");
+const viewByMonthBtn = document.getElementById("viewByMonth");
+
